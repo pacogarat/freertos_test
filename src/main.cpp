e6b1d94911c4a49ad54fcc0d9fcaddca7881fe34
pacogarat <f.garat@sumauto.com>
2023-01-24 15:48:35 +0100

    funcionando

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,15 @@
 #ifndef WebSerial
   #include <WebSerial.h>
 #endif
+#include "DHTesp.h" // Click here to get the library: http://librarymanager/All#DHTesp
 
 WifiTask* wifi = new WifiTask("MiFibra-4980", "5jXdUfg4");
 OTATask* ota = new OTATask();
 WebSerialWrapper* webserial = new WebSerialWrapper();
+DHTesp dht;
+
+const int dhtPin = 17;
+
 
 void onMessage(uint8_t *data, size_t len)
 {
@@ -38,16 +43,41 @@ void onMessage(uint8_t *data, size_t len)
   }
 }
 
+void setupDHT11() {
+	dht.setup(dhtPin, DHTesp::DHT11);
+	WebSerial.println("DHT initiated");
+}
+
+bool getTemperature() {
+	// Reading temperature for humidity takes about 250 milliseconds!
+	// Sensor readings may also be up to 2 seconds 'old' (it's a very slow sensor)
+  TempAndHumidity newValues = dht.getTempAndHumidity();
+	// Check if any reads failed and exit early (to try again).
+	if (dht.getStatus() != 0) {
+		WebSerial.print("DHT11 error status: ");
+    WebSerial.println(dht.getStatusString());
+		return false;
+	}
+
+  WebSerial.print(" T: ");
+  WebSerial.println(newValues.temperature);
+  WebSerial.print(" H: "); 
+  WebSerial.println(newValues.humidity);
+	return true;
+}
+
 void setup()
 {
   Serial.begin(115200);
   wifi->setup();
   ota->setup();
   webserial->setup(onMessage);
+  setupDHT11();
 }
 
 void loop()
 {
   ota->loop();
-  delay(100);
+  getTemperature();
+  delay(5000);
 }
